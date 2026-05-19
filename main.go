3970daa827e7af6e77c804c2b5e3589778c342e5
Suyunjing <Susus0175@proton.me>
2026-05-19 21:18:54 +0800

    Change DNS server address to 127.0.0.21:53

--- main.go
+++ main.go
@@ -708,10 +708,10 @@ func main() {
 	// UDP
 	go func() {
 		server := &dns.Server{
-			Addr: ":5353",
+			Addr: "127.0.0.21:53",
 			Net:  "udp",
 		}
-		log.Println("DNS server started on UDP :5353")
+		log.Println("DNS server started on UDP 127.0.0.21:53")
 		if err := server.ListenAndServe(); err != nil {
 			log.Fatalf("UDP server failed: %v", err)
 		}
@@ -719,10 +719,10 @@ func main() {
 
 	// TCP
 	server := &dns.Server{
-		Addr: ":5353",
+		Addr: "127.0.0.21:53",
 		Net:  "tcp",
 	}
-	log.Println("DNS server started on TCP :5353")
+	log.Println("DNS server started on TCP 127.0.0.21:53")
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("TCP server failed: %v", err)
 	}
